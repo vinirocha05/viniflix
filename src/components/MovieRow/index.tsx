f60b5a3805e34b222c335931b353758856db3e44
Vinicius Rocha <vinirocha05@gmail.com>
2022-08-23 21:07:19 -0300

    addin button controller

--- src/components/MovieRow/index.tsx
+++ src/components/MovieRow/index.tsx
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import { Movie } from '../../domain/Movie';
 import MovieCard from '../MovieCard';
-import { ListContainer, Content } from './styles';
+import { ListContainer, Content, ButtonLeft, ButtonRight } from './styles';
 
 export type MovieRowProps = {
   movies: Movie[];
@@ -8,13 +9,30 @@ export type MovieRowProps = {
 };
 
 export default function MovieRow({ movies, genre }: MovieRowProps) {
+  const [margin, setMargin] = useState(0);
+  const moveRight = () => {
+    if (margin < -2400) return;
+    const updatedMargin = margin - 300;
+    setMargin(updatedMargin);
+  };
+
+  const moveLeft = () => {
+    if (margin > -300) return;
+
+    const updatedMargin = margin + 300;
+    setMargin(updatedMargin);
+  };
+
   return (
     <Content>
       <h2>{genre}</h2>
-      <ListContainer>
+      <ListContainer margin={margin}>
+        <ButtonLeft onClick={moveLeft}>{'<'}</ButtonLeft>
+
         {movies.map((e) => (
           <MovieCard movie={e} key={e.id} />
         ))}
+        <ButtonRight onClick={moveRight}>{'>'}</ButtonRight>
       </ListContainer>
     </Content>
   );
--- src/components/MovieRow/styles.ts
+++ src/components/MovieRow/styles.ts
@@ -1,8 +1,40 @@
 import styled from 'styled-components';
 
-export const ListContainer = styled.section`
+interface ListContainerProps {
+  margin: number;
+}
+
+export const ListContainer = styled.section<ListContainerProps>`
   display: flex;
   overflow-x: hidden;
+  margin-left: ${({ margin }) => `${margin}px`};
+  align-items: center;
+  font-size: 4rem;
+  transition: all ease-in-out 200ms;
+`;
+
+export const ButtonLeft = styled.span`
+  position: absolute;
+  left: 5px;
+  z-index: 2;
+  padding: 2rem;
+  border-radius: 4rem;
+  cursor: pointer;
+  :hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+`;
+
+export const ButtonRight = styled.span`
+  position: absolute;
+  right: 5px;
+  z-index: 2;
+  padding: 2rem;
+  border-radius: 4rem;
+  cursor: pointer;
+  :hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
 `;
 
 export const Content = styled.div`
